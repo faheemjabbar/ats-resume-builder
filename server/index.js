902b25224d82dcf7d71c13507fb668a0a524287f
faheemjabbar <faheemjabbar36@gmail.com>
2025-09-02 17:08:59 +0500

    Initial commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true
 }));
 
